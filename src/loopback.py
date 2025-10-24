6f82691525ec8c32e8baf43569dc91e1d56e69d2
Mingqian <126813426+MingqianDev@users.noreply.github.com>
2025-10-24 01:35:29 -0400

    add the entire process to loopback

--- src/loopback.py
+++ src/loopback.py
@@ -1,4 +1,4 @@
-from machine import Pin, PWM, I2C, UART
+from machine import Pin, PWM, I2C, UART, ADC
 import time
 from lib.ads1x15 import ADS1015
 
@@ -7,8 +7,11 @@ i2c = I2C(1, scl=Pin(15), sda=Pin(14))
 adc = ADS1015(i2c, address=0x48)
 pwm = PWM(Pin(16), freq=1000) # Connect Pin 16 to PWM
 
+r1 = ADC(Pin(26))
+
 # init UART
 uart = UART(1, baudrate=9600, tx=Pin(8), rx=Pin(9)) # connect tx and rx together
+
 
 def loopback_pwm():
     for duty in range(0, 65536 + 8192, 8192):
@@ -37,3 +40,37 @@ def loopback_uart():
                     print("Could not interpret received data.")
 
 
+def loopback_whole():
+    # set the duty cycle
+
+    while True:
+        duty = r1.read_u16()
+        pwm.duty_u16(duty)
+        print("PWM duty cycle set to: ", duty / 65535 * 100, "%")
+
+        # send data
+        uart.write(str(duty) + "\n")
+
+        # read the signal 
+        raw = adc.read(channel1=2)  # read AIN2
+        voltage = adc.raw_to_v(raw)
+        print("ADC voltage:", voltage, "V")
+
+        if uart.any():
+            line = uart.readline() # read the line from the UART
+            if line:
+                try:
+                    text = line.strip() # strip the whitespace
+                    received_value = int(text) # between 0-65535
+                    print("Received:", received_value)
+
+                    error = abs(received_value - raw) / received_value * 100 # calculate the error as a percentage
+                    print("Error:", error, "%")
+                except ValueError:
+                    print("Could not interpret received data.")
+
+        print("-------------------------------------")
+        
+        time.sleep(1)
+
+loopback_whole()
